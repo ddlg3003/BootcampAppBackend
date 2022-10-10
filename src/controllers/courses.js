4ff752933a42f80ca46d9e54ac8d5cc5a8ab5881
NGUYỄN HỮU ĐĂNG <ducbmt2009@gmail.com>
2022-10-10 17:24:36 +0700

    CRUD on course

--- src/controllers/courses.js
+++ src/controllers/courses.js
@@ -57,3 +57,40 @@ export const createCourse = asyncHandler(async (req, res, next) => {
         data: course,
     })
 });
+
+// @desc    Update course 
+// @route   [PUT] /api/v1/courses/:id
+// @access  Private
+export const updateCourse = asyncHandler(async (req, res, next) => {
+    const { bootcamp, ...data } = req.body;
+
+    const course = await Course.findByIdAndUpdate(req.params.id, data, {
+        new: true,
+        runValidators: true,
+    });
+
+    if(!course) return next(new ErrorResponse(`No course with id of ${req.params.id}`, 404));
+
+    res.status(200).json({
+        success: true,
+        data: course,
+    })
+});
+
+// @desc    Delete course 
+// @route   [DELETE] /api/v1/courses/:id
+// @access  Private
+export const deleteCourse = asyncHandler(async (req, res, next) => {
+
+    const course = await Course.findById(req.params.id);
+
+    if(!course) return next(new ErrorResponse(`No course with id of ${req.params.id}`, 404));
+
+    await course.remove();
+
+    res.status(200).json({
+        success: true,
+        data: {},
+    })
+});
+
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -2,14 +2,16 @@ import express from 'express';
 import { 
     getCourses,
     getCourse,
-    createCourse
+    createCourse,
+    updateCourse,
+    deleteCourse
 } from '../controllers/courses.js';
 
 // mergeParams will reserve the params from the parent router pass to this router
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(createCourse);
-router.route('/:id').get(getCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 export default router;
 
